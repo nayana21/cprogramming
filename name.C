7d68bea8e733944c24c1fa6e80d9403c1888af25
nayana21 <62895110+nayana21@users.noreply.github.com>
2020-03-31 08:11:11 -0700

    assignment1b

---
+++ name.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name:Nayana Girish\n");
+    printf("Department:Electronics and Communication Engineering\n");
+    printf("College:College of Engineering,Chengannur");
+    return 0;
+}
