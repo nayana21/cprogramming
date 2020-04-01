3a66ad9cda265df5da88f62d0b760c397e4e3267
nayana girish <62895110+nayana21@users.noreply.github.com>
2020-04-01 08:59:12 -0700

    assignment 2a

---
+++ dat.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char nameStudent[] = "Anu";
+    char nameSchool[] = "AVM";
+    char classStudent[] = "Tenth";
+    int ageStudent = 15;
+    int markStudent = 40.0;
+    printf("The name of the student is %s\n", nameStudent);
+    printf("She is studying at %s\n", nameSchool);
+    printf("Her age is %d\n", ageStudent);
+    printf("She is studying in %s standard\n", classStudent);
+    printf("She got %d marks in the exam\n", markStudent);
+    return 0;
+}
